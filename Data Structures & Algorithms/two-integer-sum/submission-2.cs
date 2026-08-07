9064fb099c4f9d770c62d59c4e6d2deea7027729
Sebastian Ramirez Vega <118126021+sebramvega@users.noreply.github.com>
2026-08-06 20:24:00 -0500

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i+1; j < nums.Length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        throw new InvalidOperationException("No two numbers add up to the target.");
+    }
+}
